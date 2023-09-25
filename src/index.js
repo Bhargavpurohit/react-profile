478dc2792d74abc2c5aeb3829849cf476405adc2
bhargavpurohit <bhargavpurohit3@gmail.com>
2023-09-25 19:11:50 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import App from "./App";
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCYr2Lv4OMqlWH2D5ca4UgSFHbrzzwsNaA",
+  authDomain: "bhargavpurohit-9ecdb.firebaseapp.com",
+  projectId: "bhargavpurohit-9ecdb",
+  storageBucket: "bhargavpurohit-9ecdb.appspot.com",
+  messagingSenderId: "553113080957",
+  appId: "1:553113080957:web:7ef37d3ba10afc301c4d14",
+  measurementId: "G-PE9LNZ9M2B",
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
