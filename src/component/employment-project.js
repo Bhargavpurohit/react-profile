0da5f8a546fba9b03bdbbac1025b21c632d55808
bhargavpurohit <bhargavpurohit3@gmail.com>
2023-09-26 10:10:42 +0530

    changes and build

--- src/component/employment-project.js
+++ src/component/employment-project.js
@@ -1,11 +1,15 @@
 import "../css/employment/employment-organisation.css";
 export default function EmploymentProject(prop) {
   const project = prop.project;
-
+  const endDate = project.isPresent
+    ? "Present"
+    : `${project.endMonth}-${project.endYear}`;
   const skillList = project.skills.map((s) => {
     return <span className="skill-list"> {s} </span>;
   });
-
+  const roleList = project.tasks.map((t) => {
+    return <li>{t}</li>;
+  });
   return (
     <div className="employment-organisation">
       <div className="organisation-name">
@@ -20,11 +24,18 @@ export default function EmploymentProject(prop) {
           <i className="fa-solid fa-briefcase"></i> {project.designation}
         </h4>
       </div>
-      {/* <div className="organisation-duration">
+      <div className="organisation-duration">
         <p>
-          Worked From {org.startMonth}-{org.startYear} To {endDate}{" "}
+          Worked From {project.startMonth}-{project.startYear} To {endDate}
         </p>
-      </div> */}
+      </div>
+      <div className="project-description">
+        <p>{project.description}</p>
+      </div>
+      <div className="project-description">
+        <h4>Role:</h4>
+        {roleList}
+      </div>
       <h4>Skills</h4>
       <div className="skills">{skillList}</div>
     </div>
